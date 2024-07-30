db50eef3dc6f4f1f3430e02b734dcd32ad0efd8e
Motohiro Yamagishi <motohiro993@gmail.com>
2024-07-30 13:49:22 +0900

    refactor: 不要なlink削除

--- src/app/contents/layout.tsx
+++ src/app/contents/layout.tsx
@@ -7,10 +7,6 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         integrity="sha384-KiWOvVjnN8qwAZbuQyWDIbfCLFhLXNETzBQjA/92pIowpC0d2O3nppDGQVgwd2nB"
         crossOrigin="anonymous"
       />
-      <link
-        href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.9.0/themes/prism-tomorrow.min.css"
-        rel="stylesheet"
-      />
       <article className="prose prose-xl m-auto py-12">{children}</article>
     </>
   );
