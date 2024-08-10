596e4c07b146cf471d594d15691ce974cad1800e
Motohiro Yamagishi <motohiro993@gmail.com>
2024-08-10 23:47:22 +0900

    refactor: 不要箇所削除

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,24 +35,12 @@ export default function Page() {
     .readdirSync(CONTENT_DIRECTORY)
     .filter((file) => !file.startsWith("_") && !file.endsWith(".tsx"));
 
+  console.log(pathNames);
+
   return (
     <main className="flex-1 grid justify-center max-w-screen-xl gap-10 p-4 md:p-8 mx-auto">
       <div className="space-y-8">
-        {[
-          {
-            title: "The ",
-            author: "John Doe",
-            date: "April 15, 2023",
-            content: "In this article",
-          },
-          {
-            title: "The",
-            author: "John Doe",
-            date: "April 15, 2023",
-            content:
-              "In this article, we explore the latest trends and technologies shaping the future of web development. From the rise of serverless architectures to the increasing importance of accessibility and performance, we dive into the key advancements that will define the next generation of web applications.",
-          },
-        ].map(({ title, date, content }, i) => (
+        {[].map(({ title, date, content }, i) => (
           <ArticleLink
             href={`/contents/${pathNames[i]}`}
             key={title}
