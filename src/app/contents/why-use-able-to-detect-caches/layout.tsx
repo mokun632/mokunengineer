7ffdb5fd2f7a6be907d0f62c002c1988d2f372c2
Motohiro Yamagishi <motohiro993@gmail.com>
2024-08-15 23:49:19 +0900

    fix: buildエラー修正

--- src/app/contents/why-use-able-to-detect-caches/layout.tsx
+++ src/app/contents/why-use-able-to-detect-caches/layout.tsx
@@ -1,14 +1,6 @@
 import type { Metadata } from "next";
 import { ContentInfoType } from "../_lib/contents-info";
 
-export const contentInfo: ContentInfoType = {
-  pathName: "why-use-able-to-detect-caches",
-  title: "なぜ、useはPromiseのキャッシュ判定ができるのか",
-  date: "2024-08-11",
-  content:
-    "useは無限ループが起きてしまうので、渡されたPromiseがレンダリング間で変更されたか検知する事ができます。どうやって検知しているかを内部のコードを追いながら、見ていきます。",
-};
-
 export const metadata: Metadata = {
   title: "なぜ、useはPromiseのキャッシュ判定ができるのか",
   description:
