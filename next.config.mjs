8eb9db7a8e9f1eb540016c462eadaba4c4764c56
Motohiro Yamagishi <motohiro993@gmail.com>
2024-08-18 18:17:48 +0900

    refactor: 不要なファイル、記述、ライブラリの削除

--- next.config.mjs
+++ next.config.mjs
@@ -2,36 +2,7 @@ import createMDX from "@next/mdx";
 import remarkGfm from "remark-gfm";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
-import rehypePrettyCode from "rehype-pretty-code";
-import { visit } from "unist-util-visit";
 import remarkBreaks from "remark-breaks";
-
-const preProcess = () => (tree) => {
-  visit(tree, (node) => {
-    if (node?.type === "element" && node?.tagName === "pre") {
-      const [codeEl] = node.children;
-
-      if (codeEl.tagName !== "code") return;
-
-      node.raw = codeEl.children?.[0].value;
-    }
-  });
-};
-
-const postProcess = () => (tree) => {
-  visit(tree, (node) => {
-    if (node?.type === "element" && node?.tagName === "figure") {
-      if (!("data-rehype-pretty-code-figure" in node.properties)) {
-        return;
-      }
-      for (const child of node.children) {
-        if (child.tagName === "pre") {
-          child.properties["raw"] = node.raw;
-        }
-      }
-    }
-  });
-};
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -42,19 +13,7 @@ const nextConfig = {
 const withMDX = createMDX({
   extension: /\.mdx?$/,
   options: {
-    remarkPlugins: [remarkGfm, remarkMath, remarkBreaks],
-    transpilePackages: ['next-mdx-remote'],
-    rehypePlugins: [
-      rehypeKatex,
-      preProcess,
-      [
-        rehypePrettyCode,
-        {
-          theme: "github-dark-dimmed",
-        },
-      ],
-      postProcess,
-    ],
+    transpilePackages: ["next-mdx-remote"],
   },
 });
 
